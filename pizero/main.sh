fd385da8282c1f5637925ceaa1a32869c87c50b4
James Bond <introbond.dev@gmail.com>
2024-05-22 17:48:02 +0700

    Clean

--- pizero/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://unofficial-builds.nodejs.org/download/release/v14.13.0/node-v14.13.0-linux-armv6l.tar.xz
-tar xvfJ node-v14.13.0-linux-armv6l.tar.xz
-sudo cp -R node-v14.13.0-linux-armv6l/* /usr/local
-rm -rf node-*
-sudo reboot
-node -v && npm -v
